5ced008dcc39372cb393554041bb9edbc7f05c73
conmathukhong27 <64211566+conmathukhong27@users.noreply.github.com>
2024-01-31 02:43:40 +0700

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,12 @@
+
+data_lake = {
+  "uri": "mongodb://localhost:27017",
+  "database": "data_scraping"
+  }
+data_warehouse = {
+  "host": "localhost",
+  "database": "postgres",
+  "user": "postgres",
+  "password": "123"
+  }
+web_driver = "C:\chromedriver\chromedriver.exe"
